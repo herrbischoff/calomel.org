993bc4526f6e443395bc6d1a5bbb20a4fc1ad58b
Marcel Bischoff <marcel@herrbischoff.com>
2017-06-14 22:00:07 +0200

    Add update script

---
+++ freebsd_network_tuning/update.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import urllib2
+from bs4 import BeautifulSoup
+
+page = urllib2.urlopen('https://calomel.org/freebsd_network_tuning.html')
+#  f = open('html.txt')
+#  page = f.read()
+
+soup = BeautifulSoup(page, 'html.parser')
+
+def saveContent(filename):
+    pre = headline.findNext('pre')
+    data = pre.text.splitlines(True)
+    with open(filename, 'w') as fout:
+        fout.writelines(data[1:])
+        fout.close()
+
+for headline in soup.find_all('h3'):
+    if "/boot/loader.conf" in headline.text:
+        saveContent('boot/loader.conf')
+    if "/etc/sysctl.conf" in headline.text:
+        saveContent('etc/sysctl.conf')
+    if "OPTIONAL: Enable the Pf firewall" in headline.text:
+        saveContent('etc/rc.conf')
